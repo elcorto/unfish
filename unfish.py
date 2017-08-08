1f2c0a9bad72c5eba42c94b0a0ad4b9ea953c980
Steve Schmerler <git@elcorto.com>
2017-08-08 21:59:26 +0200

    ENH: unfish: add 'prep' subcommand, add rms_db and -r/--max-rms

--- unfish.py
+++ unfish.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 
 import numpy as np
-import cv2
+import cv2, json
 import os, sys, argparse
 import PIL.Image
 pj = os.path.join
@@ -58,7 +58,6 @@ def calibrate(img_names, fraction=0.2, maxiter=30, tol=0.1, pattern_size=(9,6)):
         # coordinates of `img`
         found, corners = cv2.findChessboardCorners(img, pattern_size)
         if found:
-            print("refine")
             # corners_fine: scale up here and refine on scaled coords,
             # should gain some small percent in precision, but not much
             corners_fine = corners.copy() * img_points_scale
@@ -73,7 +72,6 @@ def calibrate(img_names, fraction=0.2, maxiter=30, tol=0.1, pattern_size=(9,6)):
             h_old, w_old = h, w
             # XXX useless, list of the same array over and over!! optimize!!!
             pattern_points_lst.append(pattern_points)
-            print('ok')
         else:
             print('chessboard not found')
             continue
@@ -82,32 +80,35 @@ def calibrate(img_names, fraction=0.2, maxiter=30, tol=0.1, pattern_size=(9,6)):
     # XXX only if we use drawChessboardCorners() !!!
     cv2.destroyAllWindows()    
 
-    print("calibrateCamera")
-    # int stuff only when scaling, could also simply use the orig w and h of
-    # the orig images :)
-    size = (int(round(w*scale_w)), int(round(h*scale_h)))
-    # CALIB_RATIONAL_MODEL: more complex model with 8 parameters instead of 5
-    # (default), gives much better results
-    rms, camera_matrix, coeffs, rvecs, tvecs = cv2.calibrateCamera(pattern_points_lst,
-                                                                   img_points_lst, 
-                                                                   size,
-                                                                   None,
-                                                                   None, None,
-                                                                   None,
-                                                                   cv2.CALIB_RATIONAL_MODEL,
-                                                                   term)
-    # XXX not sure about the math here, also only operations on numpy arrays
-    # below, make simpler
-    mean_error = 0
-    for i in range(len(pattern_points_lst)):
-	imgpoints2, _ = cv2.projectPoints(pattern_points_lst[i], rvecs[i], tvecs[i], camera_matrix, coeffs)
-	error = cv2.norm(img_points_lst[i],imgpoints2.reshape(-1,2), cv2.NORM_L2)/len(imgpoints2)
-	mean_error += error
-    mean_error = mean_error / len(pattern_points_lst)
-    
-    return {'camera_matrix': camera_matrix, 'rms': rms, 'coeffs': coeffs,
-            'mean_error': mean_error}
-
+    if len(pattern_points_lst) > 0:
+        print("calibrateCamera")
+        # int stuff only when scaling, could also simply use the orig w and h of
+        # the orig images :)
+        size = (int(round(w*scale_w)), int(round(h*scale_h)))
+        # CALIB_RATIONAL_MODEL: more complex model with 8 parameters instead of 5
+        # (default), gives much better results
+        rms, camera_matrix, coeffs, rvecs, tvecs = cv2.calibrateCamera(pattern_points_lst,
+                                                                       img_points_lst, 
+                                                                       size,
+                                                                       None,
+                                                                       None, None,
+                                                                       None,
+                                                                       cv2.CALIB_RATIONAL_MODEL,
+                                                                       term)
+        # XXX not sure about the math here, also only operations on numpy arrays
+        # below, make simpler
+        print("calculate error")
+        mean_error = 0
+        for i in range(len(pattern_points_lst)):
+            imgpoints2, _ = cv2.projectPoints(pattern_points_lst[i], rvecs[i], tvecs[i], camera_matrix, coeffs)
+            error = cv2.norm(img_points_lst[i],imgpoints2.reshape(-1,2), cv2.NORM_L2)/len(imgpoints2)
+            mean_error += error
+        mean_error = mean_error / len(pattern_points_lst)
+        
+        return {'camera_matrix': camera_matrix, 'rms': rms, 'coeffs': coeffs,
+                'mean_error': mean_error}
+    else:
+        return None
 
 def apply(img_names, dr='converted'):
     # EXIF: cv2.imread() / cv2.imwrite() use plain numpy 3d arrays, we need
@@ -119,12 +120,11 @@ def apply(img_names, dr='converted'):
     # all infos in a dict and pickle to disk, or use a hdf5 file
     camera_matrix = np.load('camera_matrix.npy')
     coeffs = np.load('coeffs.npy')
-    old_size_src = None
-    cm = {}
 
     if not os.path.exists(dr):
         os.makedirs(dr)
 
+    cm = {}
     for ifn,fn in enumerate(img_names):
         tgt = pj(dr, os.path.basename(fn))
         if os.path.exists(tgt):
@@ -146,36 +146,72 @@ def apply(img_names, dr='converted'):
                                                  None, cm[size_src]['cm'], size_src,
                                                  cv2.CV_32FC1)
         
-        dst = cv2.remap(src, mapx, mapy, cv2.INTER_LINEAR)
-        im = PIL.Image.fromarray(dst)
+        im = PIL.Image.fromarray(cv2.remap(src, mapx, mapy, cv2.INTER_LINEAR))
         im.save(tgt, exif=pil_img.info["exif"])
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+  
     subparsers = parser.add_subparsers(dest='sub')
+
+    p_prepare = subparsers.add_parser('prep', 
+                                       help='prepare calibration run, '
+                                            'calculate rms error per calib image')
+    p_prepare.add_argument('files', nargs='+')
+    
     p_calibrate = subparsers.add_parser('calib', 
                                         help='calibrate using chessboard '
                                              'images')
     p_calibrate.add_argument('files', nargs='+')
-    
+    p_calibrate.add_argument('-r', '--max-rms', default=None, type=float,
+                             help='maximal allowed RMS error for calib image')
+     
+  
     p_apply = subparsers.add_parser('apply', 
                                     help='apply corrections to images'
                                          'images')
     p_apply.add_argument('files', nargs='+')
     p_apply.add_argument('dir', default='corrected', nargs='?', 
                          help='target dir for corrected images [%(default)s]')
+   
+    for sub in [p_prepare, p_calibrate]:
+        sub.add_argument('-f', '--fraction', default=1.0, type=float, 
+                         help='factor by which calib images are smaller '
+                              'than to-be-processed real images '
+                              '[%(default)s]')
+  
     args = parser.parse_args(sys.argv[1:])
     # chessboard pics for calibration
     # ./this.py calibrate chess_pics/small/*
     # ./this.py apply real_pics/orig/* 
     
     if args.sub == 'calib':
-        ret = calibrate(args.files)
-        print "mean error: ", ret['mean_error']
-        print "RMS:", ret['rms']
-        np.save('camera_matrix.npy', ret['camera_matrix'])
-        np.save('coeffs.npy', ret['coeffs'])
+        if args.max_rms:
+            with open('rms_db.json') as fd:
+                rms_db = json.load(fd)
+            files = [k for k,v in rms_db.items() if v < args.max_rms] 
+        else:
+            files = args.files
+        ret = calibrate(files, fraction=args.fraction)
+        if ret:
+            print "mean error: ", ret['mean_error']
+            print "RMS:", ret['rms']
+            np.save('camera_matrix.npy', ret['camera_matrix'])
+            np.save('coeffs.npy', ret['coeffs'])
+        else:
+            print("unable to process data")
+    elif args.sub == 'prep':
+        rms_db = {}
+        for fn in args.files:
+            ret = calibrate([fn], fraction=args.fraction)
+            if ret:
+                rms_db[fn] = ret['rms']
+                print("{fn}: {rms}".format(fn=fn, rms=ret['rms']))
+            else:
+                print("#{fn}: no data".format(fn=fn))
+        with open('rms_db.json', 'w') as fd:
+            json.dump(rms_db, fd, indent=2)
     elif args.sub == 'apply':
         apply(args.files, dr=args.dir)
 
